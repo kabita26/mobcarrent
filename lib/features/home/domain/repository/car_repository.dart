56958e005d48fec01cd371d3a009e7e998638129
kabita26 <mahatok008@gmail.com>
2025-02-14 19:24:10 +0545

    Car Repository

---
+++ lib/features/home/domain/repository/car_repository.dart
@@ -0,0 +1,7 @@
+
+abstract class CarRepository {
+  Future<List<Car>> getCarListings();
+  Future<List<Car>> getWishlist();
+  Future<List<Car>> getBookings();
+  Future<List<Car>> searchCars(String query);
+}
