3d67e6da7dd3230ea4afb8816d1a5f7dbc3fb888
kabita26 <mahatok008@gmail.com>
2025-02-17 19:32:06 +0545

    Update Search cubit

---
+++ lib/features/home/presentation/view_model/search_cubit.dart
@@ -0,0 +1,42 @@
+import 'package:bloc/bloc.dart';
+import '../../domain/use_case/search_cars.dart';
+
+class SearchState {
+  final List<Car> results;
+  final bool isLoading;
+  final String error;
+
+  SearchState({
+    required this.results,
+    required this.isLoading,
+    required this.error,
+  });
+
+  factory SearchState.initial() {
+    return SearchState(results: [], isLoading: false, error: '');
+  }
+
+  SearchState copyWith({List<Car>? results, bool? isLoading, String? error}) {
+    return SearchState(
+      results: results ?? this.results,
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+    );
+  }
+}
+
+class SearchCubit extends Cubit<SearchState> {
+  final GetSearchResults getSearchResults;
+
+  SearchCubit(this.getSearchResults) : super(SearchState.initial());
+
+  Future<void> search(String query) async {
+    emit(state.copyWith(isLoading: true));
+    try {
+      final results = await getSearchResults(query);
+      emit(state.copyWith(results: results, isLoading: false));
+    } catch (e) {
+      emit(state.copyWith(error: e.toString(), isLoading: false));
+    }
+  }
+}
