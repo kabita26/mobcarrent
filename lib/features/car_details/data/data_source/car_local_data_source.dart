459e2a603c041659df325f831dd6547fa2bb411c
kabita26 <mahatok008@gmail.com>
2025-02-23 23:49:45 +0545

    Car Local data

--- lib/features/car_details/data/data_source/car_local_data_source.dart
+++ lib/features/car_details/data/data_source/car_local_data_source.dart
@@ -1,34 +1,34 @@
-// import 'package:car_rent/core/network/hive_service.dart';
-// import 'package:car_rent/features/car_details/data/data_source/car_data_source.dart';
-// import 'package:car_rent/features/car_details/data/model/car_hive_model.dart';
-// import 'package:car_rent/features/car_details/domain/entity/car_entity.dart';
+import 'package:car_rent/core/network/hive_service.dart';
+import 'package:car_rent/features/car_details/data/data_source/car_data_source.dart';
+import 'package:car_rent/features/car_details/data/model/car_hive_model.dart';
+import 'package:car_rent/features/car_details/domain/entity/car_entity.dart';
 
-// class CarLocalDataSource implements ICarDataSource {
-//   final HiveService hiveService;
+class CarLocalDataSource implements ICarDataSource {
+  final HiveService hiveService;
 
-//   CarLocalDataSource({required this.hiveService});
+  CarLocalDataSource({required this.hiveService});
 
-//   // Get all cars
-//   @override
-//   Future<List<CarEntity>> getCar() async {
-//     try {
-//       final carsHiveModels = await hiveService.getAllCares();
-//       return carsHiveModels.map((e) => e.toEntity()).toList();
-//     } catch (e) {
-//       throw Exception('Failed to load cars: $e');
-//     }
-//   }
+  // Get all cars
+  @override
+  Future<List<CarEntity>> getCar() async {
+    try {
+      final carsHiveModels = await hiveService.getAllCares();
+      return carsHiveModels.map((e) => e.toEntity()).toList();
+    } catch (e) {
+      throw Exception('Failed to load cars: $e');
+    }
+  }
 
-//   // Get car by ID
-//   Future<CarEntity?> getCarById(String carId) async {
-//     try {
-//       final carHiveModel = await hiveService.getCarById(carId);
-//       if (carHiveModel != null) {
-//         return carHiveModel.toEntity();
-//       }
-//       return null; // If no car is found with the provided ID
-//     } catch (e) {
-//       throw Exception('Failed to load car with ID $carId: $e');
-//     }
-//   }
-// }
+  // Get car by ID
+  Future<CarEntity?> getCarById(String carId) async {
+    try {
+      final carHiveModel = await hiveService.getCarById(carId);
+      if (carHiveModel != null) {
+        return carHiveModel.toEntity();
+      }
+      return null; // If no car is found with the provided ID
+    } catch (e) {
+      throw Exception('Failed to load car with ID $carId: $e');
+    }
+  }
+}
--- lib/features/car_details/data/data_source/remote_datasource/car_remote_datasource.dart
+++ lib/features/car_details/data/data_source/remote_datasource/car_remote_datasource.dart
@@ -1,21 +1,21 @@
-import 'package:car_rent/features/car_details/data/data_source/car_data_source.dart';
-import 'package:car_rent/features/car_details/domain/entity/car_entity.dart';
-import 'package:dio/dio.dart';
+// import 'package:car_rent/features/car_details/data/data_source/car_data_source.dart';
+// import 'package:car_rent/features/car_details/domain/entity/car_entity.dart';
+// import 'package:dio/dio.dart';
 
-class CarRemoteDataSource implements ICarDataSource {
-  final Dio _dio;
+// class CarRemoteDataSource implements ICarDataSource {
+//   final Dio _dio;
 
-  // BatchRemoteDataSource(this._dio);
+//   // BatchRemoteDataSource(this._dio);
 
-  CarRemoteDataSource({
-    required Dio dio,
-  }) : _dio = dio;
+//   CarRemoteDataSource({
+//     required Dio dio,
+//   }) : _dio = dio;
 
  
 
-  @override
-  Future<List<CarEntity>> getCares() {
-    // TODO: implement deleteBatch
-    throw UnimplementedError();
-  }
-}
+//   @override
+//   Future<List<CarEntity>> getCares() {
+//     // TODO: implement deleteBatch
+//     throw UnimplementedError();
+//   }
+// }
